7fdd425e4b9a00f2739e4cb8199f8b6ce9b848ee
Khouloud <sassikhouloud45@gmail.com>
2026-01-20 19:49:24 +0100

    Create conditionals-basics2.py

---
+++ conditionals-basics2.py
@@ -0,0 +1,10 @@
+let weather = "sunny";
+let onVacation = true;
+let haveMoney = true; // We have money for the trip
+
+// Check if it's sunny, we're on vacation, and we have money
+if (weather === "sunny" && onVacation && haveMoney) {
+    console.log("Beach trip time!");
+} else {
+    console.log("No beach trip today.");
+}
