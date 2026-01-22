041480d99c789fc62a7904698fbd690f116aa69f
Khouloud <sassikhouloud45@gmail.com>
2026-01-22 18:46:38 +0100

    Create create-use4.js

---
+++ Functions/create-use4.js
@@ -0,0 +1,10 @@
+// Define a function that takes a name as a parameter and returns a greeting message
+function greetByName(name) {
+    return "Hello " + name + "!";
+}
+
+// Invoke the function with a name of your choice and store the return value in a variable
+let greetingAlice = greetByName("Alice");
+
+// Print out the variable containing the greeting
+console.log(greetingAlice);
