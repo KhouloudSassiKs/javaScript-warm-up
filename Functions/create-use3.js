52bc715dee368f172f58773a7cb7f6ab264e7fbf
Khouloud <sassikhouloud45@gmail.com>
2026-01-22 18:43:01 +0100

    Create create-use3.js

---
+++ Functions/create-use3.js
@@ -0,0 +1,8 @@
+// A function that greets a friend based on the event they are attending
+function greetFriend(eventName) {
+    console.log("Hello dear friend, welcome to my " + eventName + "!");
+}
+
+// Calling the function with different event names
+greetFriend("Birthday Party");
+greetFriend("Graduation");
