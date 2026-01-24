855f4a866eefc834351670837af5b5ae7e981342
Khouloud <sassikhouloud45@gmail.com>
2026-01-24 23:44:57 +0100

    Create array-ops.js

---
+++ arrays/array-ops.js
@@ -0,0 +1,12 @@
+// Array of fruits
+let fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
+
+// Access the third fruit
+console.log(fruits[2]); // "Orange"
+
+// Update "Grapes" to "Strawberry" and add a new fruit
+fruits[3] = "Strawberry";
+fruits.push("Pineapple");
+
+console.log(fruits);
+// ["Apple", "Banana", "Orange", "Strawberry", "Mango", "Pineapple"]
